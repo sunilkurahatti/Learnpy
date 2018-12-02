82acbe75d10514657ddf462324fc4c95c3d3d6f7
sunilkurahatti <sunilkurahatti@gmail.com>
2018-12-02 09:27:20 +0530

    lines iteration

--- Practice/sushi.py
+++ Practice/sushi.py
@@ -3,4 +3,11 @@
 
 with open('new.txt','r') as f:
 	for lines in f:
+		ln=str(lines)
+		lns=[]
+		lns.append(lines)
+		lns1=ln.split(' ')
 		print(lines, end='')
+		print(lns1, end='')
+		for i in lns1:
+			print(i)
